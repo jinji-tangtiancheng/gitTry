a96734db564be1122ec57f528c1541d76550bf39
tangtiancheng <tangtianchengjinji-inc.com>
2016-12-27 01:17:26 +0800

    添加工程

---
+++ git尝试工程/git尝试工程/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git尝试工程
+//
+//  Created by 唐天成 on 2016/12/26.
+//  Copyright © 2016年 唐天成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//wocao
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git尝试工程/git尝试工程/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  git尝试工程
+//
+//  Created by 唐天成 on 2016/12/26.
+//  Copyright © 2016年 唐天成. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"1234");
+    NSLog(@"jkjkjkj");
+    NSLog(@"uyuyitit");
+    NSLog(@"hahahha");
+    NSLog(@"远程");
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"创建了一条分支");
+    NSLog(@"chaungjianleyitiaofenzhi");
+    NSLog(@"在主分支修改了");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
